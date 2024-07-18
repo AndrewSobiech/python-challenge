131c34d33f6377ceedd81b24f7794634c907789a
Andrew_Sobiech <andrewsobiech@Andrews-MBP.attlocal.net>
2024-07-18 18:51:30 -0400

    Simple update

---
+++ PyBank/main.py
@@ -0,0 +1,58 @@
+import csv
+
+# CSV file path
+file_path = "/Users/andrewsobiech/Desktop/Classwork/UNCC-VIRT-DATA-PT-06-2024-U-LOLC-main/github repositories/python-challenge/PyBank/budget_data.csv"
+
+# initialize var
+total_months = 0
+total_profit_losses = 0
+changes = []
+previous_value = None
+greatest_increase = ['Data', 0]
+greatest_decrease = ['Date', 0]
+
+# Read CSV file
+with open(file_path, mode='r') as file:
+    csv_reader = csv.reader(file)
+    header = next(csv_reader)
+
+    for row in csv_reader:
+      total_months +=1
+      current_profit_losses = int(row[1])
+      total_profit_losses += current_profit_loses
+
+      # calc changes in profits/losses
+      if previous_valie is not None:
+          change = current_profit_losses - previous_value
+          changes.append(change)
+
+          # greatest increase in profits
+          if change > greatest_increase[1]:
+              greatest_increase = [row[0], change]
+
+          #  greatest decrease in profits
+          if change < greatest_decrease[1]:
+              greatest_decrease = [row[0], change]
+
+      previous_value = current_profit_losses
+
+# Calculate average of changes
+average_change = sum(changes) / len(changes)
+
+# Print to terminal
+print("Financial Analysis")
+print(f"Total Months: {total_months}")
+print(f"Total: ${total_profit_losses}")
+print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
+print(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")
+
+# Export a text file with the results
+with open('financial_analysis_results.txt', 'w') as output_file:
+    output_file.write("Financial Analysis\n")
+    output_file.write("----------------------------\n")
+    output_file.write(f"Total Months: {total_months}\n")
+    output_file.write(f"Total: ${total_profit_losses}\n")
+    output_file.write(f"Average Change: ${average_change:.2f}\n")
+    output_file.write(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n")
+    output_file.write(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
